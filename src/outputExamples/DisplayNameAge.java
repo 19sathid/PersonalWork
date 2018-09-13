047326bd86b1b3a72544c41c5798b42ca7567868
Divya Sathiyamoorthy <19sathid@elmbrookstudents.org>
2018-09-13 13:17:59 -0500

    Display name  age

---
+++ src/outputExamples/DisplayNameAge.java
@@ -0,0 +1,11 @@
+package outputExamples;
+
+public class DisplayNameAge {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Divya Sathiyamoorthy");
+System.out.println("17");
+	}
+
+}
