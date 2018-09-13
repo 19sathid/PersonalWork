4d3d1b4685163efdd9aef3bcc7cbae10722c9013
Divya Sathiyamoorthy <19sathid@elmbrookstudents.org>
2018-09-13 14:08:30 -0500

    Dislikes and likes

---
+++ src/outputExamples/DislikesAndLikes.java
@@ -0,0 +1,17 @@
+package outputExamples;
+
+public class DislikesAndLikes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.print("\t Likes:");
+System.out.println("\t \t Dislikes:");
+System.out.print("\t 1.Chocolates");
+System.out.println("\t Donuts");
+System.out.print("\t 2.Puppies");
+System.out.println("\t Flies");
+System.out.print("\t 3.Badminton");
+System.out.println("\t Volleyball");
+	}
+
+}
