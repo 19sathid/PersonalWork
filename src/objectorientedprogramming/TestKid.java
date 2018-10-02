208e7ac56f968c8b828f133fcb1dbbc2a477d97b
Divya Sathiyamoorthy <19sathid@elmbrookstudents.org>
2018-10-02 15:05:58 -0500

    Kid

---
+++ src/objectorientedprogramming/TestKid.java
@@ -0,0 +1,15 @@
+package objectorientedprogramming;
+
+public class TestKid {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	Kid annie = new Kid();
+		
+annie.sayMama();
+annie.sayFood();
+annie.sayStink();
+
+}
+}
