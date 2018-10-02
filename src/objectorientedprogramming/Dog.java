8c762de6aed94c5aeca9274c1afe64e795f7a79a
Divya Sathiyamoorthy <19sathid@elmbrookstudents.org>
2018-10-02 14:34:16 -0500

    Dog

---
+++ src/objectorientedprogramming/Dog.java
@@ -0,0 +1,14 @@
+package objectorientedprogramming;
+
+public class Dog {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	TestDog bunny = new TestDog();
+	
+bunny.woof();
+bunny.attack();
+	
+}
+}
---
+++ src/objectorientedprogramming/TestDog.java
@@ -0,0 +1,12 @@
+package objectorientedprogramming;
+
+public class TestDog {
+
+	public TestDog(){
+	}
+	public void woof(){
+		System.out.println("Woof");;
+	}
+	public void attack(){
+		System.out.println("grrrrrr");;
+	}}
