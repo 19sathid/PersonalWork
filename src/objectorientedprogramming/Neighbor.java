970405f34be19907f6ec198d0d4034618e8fbf6f
Divya Sathiyamoorthy <19sathid@elmbrookstudents.org>
2018-10-03 14:00:20 -0500

    Neighbor

---
+++ src/objectorientedprogramming/Neighbor.java
@@ -0,0 +1,19 @@
+package objectorientedprogramming;
+
+public class Neighbor {
+
+	public Neighbor(){
+	}
+	public void sayJoke()
+	{
+		System.out.println("joke");
+	}
+	public void sayMoney()
+	{
+		System.out.println("I want my money");
+	}
+	public void sayConversation()
+	{
+		System.out.println("I will give your money tomorrow");
+	}
+	}
---
+++ src/objectorientedprogramming/TestNeighbor.java
@@ -0,0 +1,15 @@
+package objectorientedprogramming;
+
+public class TestNeighbor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	Neighbor annie = new Neighbor();
+		
+annie.sayJoke();
+annie.sayMoney();
+annie.sayConversation();
+
+}
+}
