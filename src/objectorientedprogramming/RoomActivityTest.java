69cf758ba17865c5e9e8d96c981214eecaf560f5
Divya Sathiyamoorthy <19sathid@elmbrookstudents.org>
2018-10-04 14:09:18 -0500

    Room Activity

--- src/objectorientedprogramming/RoomActivityTest.java
+++ src/objectorientedprogramming/RoomActivityTest.java
@@ -4,7 +4,13 @@ public class RoomActivityTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-	}
-
+{
+		Room closet = new Room();
+		Room kitchen = new Room();
+		kitchen = closet;
+		Room basement = new Room();
+		closet = basement;
+		closet.area();
+}
+}
 }
