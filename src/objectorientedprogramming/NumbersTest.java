e6d9dfa87040b46299f120c27332e9a8dbe6ca78
Divya Sathiyamoorthy <19sathid@elmbrookstudents.org>
2018-10-04 14:08:52 -0500

    Numbers

---
+++ src/objectorientedprogramming/NumbersTest.java
@@ -0,0 +1,15 @@
+package objectorientedprogramming;
+
+public class NumbersTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Numbers bob = new Numbers();
+bob.sayNumber(7.5);
+bob.sayNumberPlus2(7.5);
+bob.saySum(10, 10);
+		
+	}
+
+}
