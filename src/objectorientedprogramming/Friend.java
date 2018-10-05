6a1c6d49f7b01825a5d9b74db19f946e3a9b601b
19sathid <19sathid@elmbrookstudents.org>
2018-10-04 21:25:57 -0500

    Friend

--- src/objectorientedprogramming/Friend.java
+++ src/objectorientedprogramming/Friend.java
@@ -1,5 +1,12 @@
-package objectorientedProgramming;
+package objectorientedprogramming;
 
 public class Friend {
 
+	public Friend()
+{
+}
+	public void sayMessage()
+	{
+		System.out.println("Your are a great friend");
+}
 }
---
+++ src/objectorientedprogramming/TestFriend.java
@@ -0,0 +1,13 @@
+package objectorientedprogramming;
+
+public class TestFriend {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Friend bob = new Friend();
+		
+bob.sayMessage();
+}
+
+}
