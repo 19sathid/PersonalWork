7dbd9dbe1b6f910f8a2614addfccd2327bda558b
19sathid <19sathid@elmbrookstudents.org>
2018-10-04 21:53:33 -0500

    Example

---
+++ src/objectorientedprogramming/Example.java
@@ -0,0 +1,17 @@
+package objectorientedprogramming;
+
+public class Example {
+
+	public Example()
+	{
+	}
+	public void sayHello()
+	{
+		System.out.println("Hello");
+	}
+	public void sayMessage()
+	{
+		System.out.println("Message");
+	
+	}
+}
---
+++ src/objectorientedprogramming/TestExample.java
@@ -0,0 +1,18 @@
+package objectorientedprogramming;
+
+public class TestExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Greeter bob = new Greeter();
+		Friend john = new Friend();
+	
+bob.sayHello();
+bob.sayHello();
+bob.sayHello();
+john.sayMessage();
+bob.sayHello();
+	}
+
+}
