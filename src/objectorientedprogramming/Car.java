ca66a1bfd25a2403f66fc20359e1ba4c6f639b43
19sathid <19sathid@elmbrookstudents.org>
2018-10-04 22:15:33 -0500

    Car

--- src/objectorientedprogramming/Car.java
+++ src/objectorientedprogramming/Car.java
@@ -1,5 +1,20 @@
-package objectorientedProgramming;
+package objectorientedprogramming;
 
 public class Car {
 
+	public Car()
+	{
+	}
+	public void sayOdometer()
+	{
+		System.out.println("200,000 miles");
+	}
+	public void sayFlat()
+	{
+		System.out.println("My car has a flat tire.");
+	}
+	public void sayThirsty()
+	{
+		System.out.println("I need to go to a gas station, my car is out of gas.");
+	}
 }
---
+++ src/objectorientedprogramming/TestCar.java
@@ -0,0 +1,15 @@
+package objectorientedprogramming;
+
+public class TestCar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Car kit = new Car();
+		kit.sayOdometer();
+		kit.sayFlat();
+		kit.sayThirsty();
+				
+	}
+
+}
