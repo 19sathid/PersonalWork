70e0e1add86e1de5532ccc25a5e2627e9483db0a
Divya Sathiyamoorthy <19sathid@elmbrookstudents.org>
2018-10-10 15:07:32 -0500

    Worker

---
+++ src/objectorientedprogramming/Worker.java
@@ -0,0 +1,21 @@
+package objectorientedprogramming;
+
+public class Worker {
+	//instance fields
+	private int hours;
+	private double pay;
+	
+	//constructors
+	public Worker(){
+		hours = 10;
+		pay = 99.0;
+	}
+	public Worker(int newHours, double newPay){
+		hours = newHours;
+		pay = newPay;
+	}
+	// Accessor Methods
+	
+}
+		
+
