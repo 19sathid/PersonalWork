f1c32d9fdfb7554bd8d4136f8eaf7b85d379b040
19sathid <19sathid@elmbrookstudents.org>
2018-10-09 22:51:11 -0500

    Circle

---
+++ src/objectorientedprogramming/CircleTest.java
@@ -0,0 +1,14 @@
+package objectorientedprogramming;
+
+public class CircleTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	Circle bob = new Circle();
+	{
+		System.out.println(bob.getRadius());
+		System.out.println(bob.circumference());
+		System.out.println(bob.area());
+	}
+}
+}
