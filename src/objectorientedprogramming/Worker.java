ae97c601aba7f0ce7f26c8bbae46ba7eac6b51a5
19sathid <19sathid@elmbrookstudents.org>
2018-10-10 19:47:44 -0500

    Worker(edited)

--- src/objectorientedprogramming/Worker.java
+++ src/objectorientedprogramming/Worker.java
@@ -1,21 +1,40 @@
 package objectorientedprogramming;
 
 public class Worker {
-	//instance fields
+	// instance fields
 	private int hours;
 	private double pay;
-	
-	//constructors
-	public Worker(){
+
+	// constructors
+	public Worker() {
 		hours = 10;
 		pay = 99.0;
 	}
-	public Worker(int newHours, double newPay){
+
+	public Worker(int newHours, double newPay) {
 		hours = newHours;
 		pay = newPay;
 	}
-	// Accessor Methods
-	
-}
-		
 
+	// Mutator methods
+	public void setHours(int newHours) {
+		hours = newHours;
+	}
+	public void setPay(double newPay) {
+		newPay = pay + 10.5;
+	}
+
+	// Accessor Methods
+	public double getPay() {
+		return pay;
+	}
+
+	public double getHours() {
+		return hours;
+	}
+
+	public double payCheck() {
+		return hours * pay;
+	}
+
+}
---
+++ src/objectorientedprogramming/WorkerTest.java
@@ -0,0 +1,26 @@
+package objectorientedprogramming;
+
+public class WorkerTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Worker bob = new Worker();
+		System.out.println("bob pay:" + bob.getPay());
+		System.out.println("bob hours:" + bob.getHours());
+		System.out.println("bob payCheck:" + bob.payCheck());
+		bob.setHours(5);
+		bob.setPay(6.5);
+		System.out.println("box hours:" + bob.getHours());
+		System.out.println("box pay:" + bob.getPay());
+
+		bob = new Worker(6, 3.5);
+		System.out.println("bob pay:" + bob.getPay());
+		System.out.println("bob hours:" + bob.getHours());
+		System.out.println("bob payCheck:" + bob.payCheck());
+		bob.setHours(5);
+		bob.setPay(6.5);
+		System.out.println("box hours:" + bob.getHours());
+		System.out.println("box pay:" + bob.getPay());
+	}
+
+}
