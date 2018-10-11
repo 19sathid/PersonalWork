bf9c676f044ba5541f845eefef97064cdd9e2e98
Divya Sathiyamoorthy <19sathid@elmbrookstudents.org>
2018-10-11 14:11:14 -0500

    Worker(edited)

--- src/objectorientedprogramming/WorkerTest.java
+++ src/objectorientedprogramming/WorkerTest.java
@@ -10,17 +10,14 @@ public class WorkerTest {
 		System.out.println("bob payCheck:" + bob.payCheck());
 		bob.setHours(5);
 		bob.setPay(6.5);
-		System.out.println("box hours:" + bob.getHours());
-		System.out.println("box pay:" + bob.getPay());
+		System.out.println("bob hours:" + bob.getHours());
+		System.out.println("bob pay:" + bob.getPay());
 
 		bob = new Worker(6, 3.5);
 		System.out.println("bob pay:" + bob.getPay());
 		System.out.println("bob hours:" + bob.getHours());
 		System.out.println("bob payCheck:" + bob.payCheck());
-		bob.setHours(5);
-		bob.setPay(6.5);
-		System.out.println("box hours:" + bob.getHours());
-		System.out.println("box pay:" + bob.getPay());
+
 	}
 
 }
