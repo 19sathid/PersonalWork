93a7a06849e50467967a545121689669834df66d
19sathid <19sathid@elmbrookstudents.org>
2018-10-10 19:12:26 -0500

    Circle(edited2)

--- src/objectorientedprogramming/Circle.java
+++ src/objectorientedprogramming/Circle.java
@@ -23,13 +23,11 @@ public class Circle {
 		return radius;
 	}
 
-	public double circumference()
-	{
-		return 2*radius*Math.PI;
-	}
-	public double area()
-	{
-		return Math.PI*radius*radius;
-	}
+	public double circumference() {
+		return 2 * radius * Math.PI;
 	}
 
+	public double area() {
+		return Math.PI * radius * radius;
+	}
+}
