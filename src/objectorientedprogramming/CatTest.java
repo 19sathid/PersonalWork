47eef1398e6a758f9a10047a34db97c15adf01bc
Divya Sathiyamoorthy <19sathid@elmbrookstudents.org>
2018-10-11 13:47:04 -0500

    Cat(edited)

--- src/objectorientedprogramming/CatTest.java
+++ src/objectorientedprogramming/CatTest.java
@@ -5,25 +5,15 @@ public class CatTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Cat kitty = new Cat();
-		System.out.println("kitty age:" + kitty.getage());
-		System.out.println("kitty hours:" + kitty.getHours());
-		System.out.println("kitty ageCheck:" + kitty.ageCheck());
-		kitty.setHours(5);
-		kitty.setage(6.5);
-		System.out.println("box hours:" + kitty.getHours());
-		System.out.println("box age:" + kitty.getage());
-
-		kitty = new Cat(6, 3.5);
-		System.out.println("kitty age:" + kitty.getage());
-		System.out.println("kitty hours:" + kitty.getHours());
-		System.out.println("kitty ageCheck:" + kitty.ageCheck());
-		kitty.setHours(5);
-		kitty.setage(6.5);
-		System.out.println("box hours:" + kitty.getHours());
-		System.out.println("box age:" + kitty.getage());
-	}
-
-}
+		System.out.println("Age: " + kitty.getAge());
+		System.out.println("Hours of Sleep: " + kitty.getSleep());
+		kitty.setAge(7);
+		kitty.setsleepMore(7.5);
+		System.out.println("Age: " + kitty.getAge() + " years");
+		System.out.println("More hours of sleep: " + kitty.getSleep());
+		kitty.setsleepLess(6.5);
+		System.out.println("Less hours of sleep: " + kitty.getSleep());
+		System.out.println("Age after birthday: " + kitty.getBirthday());
 	}
 
 }
