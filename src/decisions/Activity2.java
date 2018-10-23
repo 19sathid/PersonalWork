a49782f99e709bd53827f0ea66033232522daef6
19sathid <19sathid@elmbrookstudents.org>
2018-10-23 14:20:26 -0500

    Activity 2

---
+++ src/decisions/Activity2.java
@@ -0,0 +1,14 @@
+package decisions;
+
+public class Activity2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int multiples = 0;
+while(multiples <= 100000){
+	System.out.println(multiples);
+	multiples = multiples + 2;
+}
+	}
+
+}
