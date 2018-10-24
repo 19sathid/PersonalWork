941ec7f0bbff12afd3ce2a4b8d788b2ef701e0ce
19sathid <19sathid@elmbrookstudents.org>
2018-10-23 19:04:36 -0500

    Activity 9(edited)

--- src/decisions/Activity9.java
+++ src/decisions/Activity9.java
@@ -6,18 +6,19 @@ public class Activity9 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-int number = 0;
-int largest = 0;
-Scanner input = new Scanner(System.in);
-System.out.println("Enter a two digit number(Enter 9999 to quit)");
-number = input.nextInt();
-while (number != 9999) {
-	if(number > largest){
-		System.out.println("Enter a two digit number");
+		int number = 0;
+		int largest = 0;
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter a two digit number(Enter 9999 to quit)");
 		number = input.nextInt();
-	}
-	System.out.println("Largest number:" + largest);
-}
+		while (number != 9999) {
+			if (number > largest) {
+				largest = number;
+			}
+				System.out.println("Enter a two digit number");
+				number = input.nextInt();
+		}
+		System.out.println("Largest number:" + largest);
 	}
 
 }
