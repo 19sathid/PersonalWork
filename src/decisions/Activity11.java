06ea49f3b2fd96134fcb37258c548e8b76fc31e9
19sathid <19sathid@elmbrookstudents.org>
2018-10-24 15:08:37 -0500

    Activity 11

---
+++ src/decisions/Activity11.java
@@ -0,0 +1,31 @@
+package decisions;
+
+import java.util.Scanner;
+
+public class Activity11 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int number = 0;
+		int largest = 0;
+		int smallest = 0;
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter an integer");
+		number = input.nextInt();
+		do {
+			if (number > largest) {
+				largest = number;
+			}
+			System.out.println("Enter a an integer(Enter 9999 to quit)");
+			number = input.nextInt();
+			if (number < smallest) {
+				smallest = number;
+			}
+
+		} while (number != 9999);
+
+		System.out.println("Range:" + (largest - smallest));
+
+	}
+
+}
