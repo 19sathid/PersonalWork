8926dd43f9772417bb10b109b726d3438870c763
19sathid <19sathid@elmbrookstudents.org>
2018-10-25 13:55:20 -0500

    Activity 13

---
+++ src/decisions/Activity13.java
@@ -0,0 +1,12 @@
+package decisions;
+
+public class Activity13 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int number = 1; number <= 19; number = number + 2) {
+			System.out.println(number + "Hello");
+		}
+	}
+
+}
