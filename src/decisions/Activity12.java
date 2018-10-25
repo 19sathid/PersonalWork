7f797626a62f8561d6d14468ed13859f73781bd1
19sathid <19sathid@elmbrookstudents.org>
2018-10-25 13:55:12 -0500

    Activity 12

---
+++ src/decisions/Activity12.java
@@ -0,0 +1,12 @@
+package decisions;
+
+public class Activity12 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int number = 1; number <= 10; number = number + 1) {
+			System.out.println(number + "Hello");
+		}
+	}
+
+}
