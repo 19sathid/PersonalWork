0be44987d82363b8f43d03f0aa064f568e286357
19sathid <19sathid@elmbrookstudents.org>
2018-10-26 11:12:32 -0500

    Count by 5

---
+++ src/decisions/Countby5.java
@@ -0,0 +1,14 @@
+package decisions;
+
+public class Countby5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int number = 5;
+		while (number < 99) {
+			System.out.println(number);
+			number = number + 5;
+		}
+	}
+
+}
