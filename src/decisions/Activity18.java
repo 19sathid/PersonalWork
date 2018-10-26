6d1bea20e7fde926669b95257b70545e88b3e996
19sathid <19sathid@elmbrookstudents.org>
2018-10-26 10:56:50 -0500

    Activity 18

---
+++ src/decisions/Activity18.java
@@ -0,0 +1,14 @@
+package decisions;
+
+public class Activity18 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int total = 0;
+		int number = 0;
+		for (number = 1; number <= 10; number++) {
+			total += number;
+		}
+		System.out.println(total);
+	}
+}
