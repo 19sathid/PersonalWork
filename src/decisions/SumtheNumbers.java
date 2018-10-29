60a70f5d28d1d77c2ccca8e466ddbaaf6e7b2831
19sathid <19sathid@elmbrookstudents.org>
2018-10-28 22:19:40 -0500

    Sum the numbers

---
+++ src/decisions/SumtheNumbers.java
@@ -0,0 +1,21 @@
+package decisions;
+
+import java.util.Scanner;
+
+public class SumtheNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num = 0;
+		int sum = 0;
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter a number(Enter 999 to quit)");
+		num = input.nextInt();
+		do {
+			System.out.println("Enter a number");
+			num = input.nextInt();
+			sum = num++;
+			System.out.println("Sum: " + sum);
+		}while (num != 999);
+	}
+}
