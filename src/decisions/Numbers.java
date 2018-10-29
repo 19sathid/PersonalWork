da5806c8f3e1771f4e143584fc100a4eef210120
19sathid <19sathid@elmbrookstudents.org>
2018-10-29 15:07:10 -0500

    Numbers

---
+++ src/decisions/Numbers.java
@@ -0,0 +1,18 @@
+package decisions;
+
+public class Numbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num = 1;
+		int num2 = 0;
+		for (num2 = 1; num2 <= 4; num2++) {
+			for (num = 1; num <= 5; num++) {
+				System.out.print(num * 1);
+			}
+			System.out.println();
+		}
+
+	}
+
+}
