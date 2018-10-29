22fe364efa4d222e4b577b2f4efa864c2c461f0a
19sathid <19sathid@elmbrookstudents.org>
2018-10-29 15:06:30 -0500

    Circle

---
+++ src/decisions/Circle.java
@@ -0,0 +1,22 @@
+package decisions;
+
+import java.util.Scanner;
+
+public class Circle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+double rad1 = 0;
+double rad2 = 0;
+double rad3 = 0;
+double perimeter = 0;
+Scanner input = new Scanner(System.in);
+System.out.println("Enter the radius of the first circle");
+rad1 = input.nextDouble();
+System.out.println("Enter the radius of the second circle");
+rad2 = input.nextDouble();
+System.out.println("Enter the radius of the third circle");
+rad3 = input.nextDouble();
+}
+
+}
