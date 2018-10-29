1d1355089ff78308252afa65ab97d0d8d51629eb
19sathid <19sathid@elmbrookstudents.org>
2018-10-29 15:06:44 -0500

    Divisors

---
+++ src/decisions/Divisors.java
@@ -0,0 +1,13 @@
+package decisions;
+
+public class Divisors {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int num = 0;
+int div = 0;
+for(num = 1; num <= 1000; num++){
+}
+	}
+
+}
