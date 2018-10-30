c034ab19001232e18e88539acb02511ed4a41d12
19sathid <19sathid@elmbrookstudents.org>
2018-10-30 15:04:51 -0500

    Class Average

---
+++ src/decisions/ClassAverage.java
@@ -0,0 +1,23 @@
+package decisions;
+
+import java.util.Scanner;
+
+public class ClassAverage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+double average = 0;
+double total = 0;
+double scores = 0;
+Scanner input = new Scanner(System.in);
+System.out.println("Enter the number of students");
+int number = input.nextInt();
+System.out.println("Enter test scores");
+scores = input.nextInt();
+	
+}
+
+
+	}
+
+
