2e330edc79b2b5408c555f5276d13010bdb86927
19sathid <19sathid@elmbrookstudents.org>
2018-10-30 15:05:56 -0500

    Factorial

---
+++ src/decisions/Factorial.java
@@ -0,0 +1,18 @@
+package decisions;
+
+import java.util.Scanner;
+
+public class Factorial {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int total = 0;
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter a positive integer");
+		int num = input.nextInt();
+		for (int counter = 1; counter <= num; counter = num - 1) {
+		}
+		System.out.println(total);
+	}
+
+}
