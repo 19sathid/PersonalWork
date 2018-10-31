947b82d4f50221fc2bde56ab2d6d3b9be75be46e
19sathid <19sathid@elmbrookstudents.org>
2018-10-31 15:08:23 -0500

    Largest Divisor

---
+++ src/decisions/LargestDivisor.java
@@ -0,0 +1,17 @@
+package decisions;
+
+public class LargestDivisor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner input = new Scanner(System.in);
+System.out.println("Enter a positive integer");
+int number = input.nextInt();
+int largest = 0;
+for (int counter = 1; counter <= number; counter++) {
+	if (number % counter == 0) {
+		
+	}
+	}
+
+}
