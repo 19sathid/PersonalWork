c2f337d358ef87cff7f1092d0f41f255308902c0
19sathid <19sathid@elmbrookstudents.org>
2018-10-31 15:08:58 -0500

    Factorial(edited)

--- src/decisions/Factorial.java
+++ src/decisions/Factorial.java
@@ -6,11 +6,13 @@ public class Factorial {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int total = 0;
+		int total = 1;
+		int factorial = 0;
 		Scanner input = new Scanner(System.in);
 		System.out.println("Enter a positive integer");
 		int num = input.nextInt();
-		for (int counter = 1; counter <= num; counter = num - 1) {
+		for (int counter = 1; counter <= num; counter++) {
+		total = total*counter;
 		}
 		System.out.println(total);
 	}
