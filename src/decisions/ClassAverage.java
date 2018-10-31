fb6f3206c41257692d9693e6e20b6f79b529b9c5
19sathid <19sathid@elmbrookstudents.org>
2018-10-31 15:07:53 -0500

    Class Average(edited)

--- src/decisions/ClassAverage.java
+++ src/decisions/ClassAverage.java
@@ -6,18 +6,19 @@ public class ClassAverage {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-double average = 0;
-double total = 0;
-double scores = 0;
-Scanner input = new Scanner(System.in);
-System.out.println("Enter the number of students");
-int number = input.nextInt();
-System.out.println("Enter test scores");
-scores = input.nextInt();
-	
-}
-
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter the number of students");
+		int number = input.nextInt();
+		int average = 0;
+		int total = 0;
+		int score = 0;
+		for (int counter = 1; counter <= number; counter++) {
+			System.out.println("Enter test score");
+			score = input.nextInt();
+			total = total + score;
+			average = total / number;
+		}
+		System.out.println("Average: " + average);
 
 	}
-
-
+}
