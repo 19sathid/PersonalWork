016d53df942f570b346bf34147fd9f7f7cbd85fb
19sathid <19sathid@elmbrookstudents.org>
2018-11-01 14:08:37 -0500

    Largest Divisor(edited)

--- src/decisions/LargestDivisor.java
+++ src/decisions/LargestDivisor.java
@@ -1,17 +1,22 @@
 package decisions;
 
+import java.util.Scanner;
+
 public class LargestDivisor {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-Scanner input = new Scanner(System.in);
-System.out.println("Enter a positive integer");
-int number = input.nextInt();
-int largest = 0;
-for (int counter = 1; counter <= number; counter++) {
-	if (number % counter == 0) {
-		
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter a positive integer");
+		int number = input.nextInt();
+		int largest = 0;
+		for (int counter = 1; counter < number; counter++) {
+			if (number % counter == 0) {
+				if (counter > largest) {
+					largest = counter;
+				}
+			}
+		}
+		System.out.println("Largest divisor: " + largest);
 	}
-	}
-
 }
