b4c4d2c753fc37d99c2f2ae5d757dec8822ed7bb
Divya <19sathid@elmbrookstudents.org>
2018-11-07 15:06:51 -0600

    Activity 1 (edited)

--- src/randomNumbers/Activity1.java
+++ src/randomNumbers/Activity1.java
@@ -3,16 +3,18 @@ package randomNumbers;
 import java.util.Random;
 
 public class Activity1 {
+	public static void main(String[]args){
 	Random generator = new Random();
 	int randomNum = 0;
-	int min = 1; // minimum random number generated
-	int max = 100000; // maximum random number generated
+	int min = 0; // minimum random number generated
+	int max = 1; // maximum random number generated
 	int heads = 0;
 	for(int counter = 1;counter<=100000;counter++)
 	{
-		randomNum = 1 + generator.nextInt(100000 - 1 + 1);
-		if(randomNum == 0)
+		randomNum = min + generator.nextInt(max - min + 1);
+		if(randomNum == 0){
 			heads++;
+		}
 	}
 System.out.println("heads = " + heads);
 System.out.println("tails = " + (100000 - heads));
