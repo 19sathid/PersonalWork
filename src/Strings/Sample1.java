8563e632fbdb9c8dff10ff7b2f2a5970b88a3384
Divya <19sathid@elmbrookstudents.org>
2018-11-12 15:07:15 -0600

    Activity 1

---
+++ src/Strings/Sample1.java
@@ -0,0 +1,14 @@
+package Strings;
+import java.util.Scanner;
+public class Sample1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner input = new Scanner(System.in);
+System.out.println("Age : ");
+int age = input.nextInt();
+System.out.println("Name : ");
+
+	}
+
+}
