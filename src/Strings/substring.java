a08895f0a3ad3092b3499a6834e8a3e2dc518c95
19sathid <19sathid@elmbrookstudents.org>
2018-11-13 15:00:18 -0600

    Substring

---
+++ src/Strings/substring.java
@@ -0,0 +1,15 @@
+package Strings;
+
+import java.util.Scanner;
+
+public class substring {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String text = "My name is Buzz Lightyear!";
+		for (int i = 0; i < text.length(); i++) {
+			System.out.println(text.substring(0,i));
+		}
+	}
+
+}
