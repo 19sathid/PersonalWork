6d5c5c0db32d6f59d8f093d80e0e63ab3ea181ff
19sathid <19sathid@elmbrookstudents.org>
2018-11-15 14:11:44 -0600

    Essay

---
+++ src/Strings/TestEssay.java
@@ -0,0 +1,15 @@
+package Strings;
+
+public class TestEssay {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Essay book = new Essay();
+		book.setName("Divya Sathiyamoorthy");
+		book.setText("Hello World");
+		System.out.println("Author's Last Name: " + book.getLastName());
+		System.out.println("There are " + book.getFirstName() + " characters in the author's first name.");
+		System.out.println("Does the author's name contain 'X'? : " + book.findX());
+	}
+
+}
