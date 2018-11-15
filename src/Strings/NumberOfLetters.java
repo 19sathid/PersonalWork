1f0b25b01f2365a3531987e6a3df9d488c386afd
19sathid <19sathid@elmbrookstudents.org>
2018-11-15 14:11:53 -0600

    Number of Letters

---
+++ src/Strings/NumberOfLetters.java
@@ -0,0 +1,19 @@
+package Strings;
+import java.util.Scanner;
+public class NumberOfLetters {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner input = new Scanner(System.in);
+int consonants = 0;
+int vowels = 0;
+int punctuation = 0;
+System.out.println("Write a sentence:");
+String sentence = input.nextLine();
+int length = sentence.length();
+for (int i = 0; i < length; i++) {
+	char character = sentence.charAt(i);
+	if(character == 'a' || character == 'e' || character == 'e' || character == 'e' ||  )
+	}
+
+}
