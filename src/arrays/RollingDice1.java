9aa91a7d647832efe7fbab9229ca713bb65aaff6
19sathid <19sathid@elmbrookstudents.org>
2018-12-03 15:03:32 -0600

    Rolling Dice Version 1

---
+++ src/arrays/RollingDice1.java
@@ -0,0 +1,19 @@
+package arrays;
+import java.util.Scanner;
+public class RollingDice1 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter the number of students in the class");
+		int num = input.nextInt();
+		System.out.println("Enter the number of sides on a die");
+		int numSides = input.nextInt();
+		int max = input.nextInt();
+		int min = 1;
+		System.out.println("Enter the number of times each student should roll");
+		int numRolls = 0;
+		
+
+	}
+
+}
