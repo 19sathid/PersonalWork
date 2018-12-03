2a9aab70826af3c4d9ce1c45ff0b57ff9efd251e
19sathid <19sathid@elmbrookstudents.org>
2018-12-03 15:01:07 -0600

    Revised Long and Short Names

--- src/arrays/LongandShortNames.java
+++ src/arrays/LongandShortNames.java
@@ -6,31 +6,44 @@ public class LongandShortNames {
 
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-				System.out.println("Enter the number of people you know");
-				int num = input.nextInt();
-				String[] names = new String[num];
-				String[] firstName = new String[num];
-				String[] lastName = new String[num];
-				for(int i = 0; i < num; i++) {
-					System.out.println("Enter the first and last names of each person");
-					names[i] = input.nextLine();
-				}
-				int longest = 0;
-				int indexOfLongest = 0;
-				for(int i = 0; i < num; i++) {
-					if(longest < names[i].length()) {
-						longest = names[i].length();
-						indexOfLongest = i;
-					}
-				}
-				String longestName = names[indexOfLongest];
-				/*	firstName[i] = names[i].substring(0, names[i].indexOf(" "));
-					System.out.println(firstName[i]);
-					lastName[i] = names[i].substring(names[i].indexOf(" ") + 1, names.length -1);
-					System.out.println(lastName);
-				*/
+		System.out.println("Enter the number of people you know");
+		int num = input.nextInt();
+		input.nextLine();
+		// clear buffer
+		String[] names = new String[num];
 
-input.close();
+		for (int i = 0; i < num; i++) {
+			System.out.println("Enter the first and last name of each person");
+			names[i] = input.nextLine();
+		}
+
+		int longest = 0;
+		int indexOfLongest = 0;
+		for (int i = 0; i < num; i++) {
+			if (longest < names[i].length()) {
+				longest = names[i].length();
+				indexOfLongest = i;
+			}
+		}
+
+		int smallest = 999;
+		int indexOfSmallest = 0;
+		for (int i = 0; i < num; i++) {
+			if (smallest > names[i].length()) {
+				smallest = names[i].length();
+				indexOfSmallest = i;
+			}
+		}
+
+		for (int i = 0; i < num; i++) {
+			System.out.println(names[i]);
+		}
+
+		System.out.println("The longest name is " + names[indexOfLongest]);
+		System.out.println("The smallest name is " + names[indexOfSmallest]);
+		System.out.println("The range of lengths of the names " + (longest - smallest));
+
+		input.close();
 	}
 
 }
