c92dcdb0611d47adc3430a1dd572977b2ff9336c
19sathid <19sathid@elmbrookstudents.org>
2018-12-05 15:09:43 -0600

    Array List

---
+++ src/arrays/ArrayLists.java
@@ -0,0 +1,39 @@
+package arrays;
+import java.util.ArrayList;
+import java.util.Scanner;
+public class ArrayLists {
+
+	public static void main(String[] args) {
+		ArrayList<String> names = new ArrayList<String>();
+		ArrayList<String> xNames = new ArrayList<String>();
+		Scanner input = new Scanner(System.in);
+		
+		for(int i = 0; i < 3; i++){
+			System.out.println("Enter a name");
+			String name = input.nextLine();
+			names.add(name);
+		}
+		for(int i = 0; i < 3; i++){
+			System.out.println(names.get(i));
+		}
+		
+		xNames.add("Bob");
+		xNames.add("Jane");
+		xNames.add(0, "Billy");
+		
+		for(int i = 0; i < 3; i++){
+			System.out.println(names.get(i));
+		}
+		
+		for(int i = 0; i < 3; i++){
+		System.out.println("Enter your name");
+		String yourName = input.nextLine();
+		names.add(yourName);
+		}
+		
+		
+		
+		input.close();
+	}
+
+}
