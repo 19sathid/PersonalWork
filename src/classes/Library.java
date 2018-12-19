b9daa7472c00db66e7d09863878552153c4c8d7d
19sathid <19sathid@elmbrookstudents.org>
2018-12-19 06:12:07 -0600

    Library

--- src/classes/Library.java
+++ src/classes/Library.java
@@ -6,19 +6,43 @@ import java.util.Random;
 
 public class Library {
 	private String name;
-	private ArrayList<Book> books;
+	private ArrayList<Book> fiction;
+
+	Scanner input = new Scanner(System.in);
+	Random rand = new Random();
 
 	public Library() {
 		name = "Brookfield Public Library";
-		Scanner input = new Scanner(System.in);
-		Random rand = new Random();
+
 		for (int i = 0; i < 10; i++) {
 			System.out.println("Enter the name of the book");
 			String title = input.nextLine();
 			int numPages = 10 + rand.nextInt(491);
-			books.add(new Book(title, numPages));
+			fiction.add(new Book(title, numPages));
 		}
 	}
-public Library(String xName, ArrayList<Book> xBook){
+
+	public Library(String xName, ArrayList<Book> xFiction) {
+		name = xName;
+		fiction = xFiction;
 	}
+
+	public void checkOut(String name) {
+		System.out.println("Enter the name of the book you want to check out");
+		String book = input.nextLine();
+		int numPages = 10 + rand.nextInt(491);
+		fiction.remove(new Book(book, numPages));
+	}
+
+	public void returnBook(String name) {
+		System.out.println("Enter the name of the book you want to return");
+		String book = input.nextLine();
+		int numPages = 10 + rand.nextInt(491);
+		fiction.add(new Book(book, numPages));
+	}
+
+	public void fire() {
+		fiction.clear();
+	}
+
 }
