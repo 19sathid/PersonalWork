6633e1d1a96995bcdf6edd6739afa3145a2b710b
19sathid <19sathid@elmbrookstudents.org>
2018-12-19 15:08:51 -0600

    Library Edited

--- src/classes/Library.java
+++ src/classes/Library.java
@@ -8,15 +8,12 @@ public class Library {
 	private String name;
 	private ArrayList<Book> fiction;
 
-	Scanner input = new Scanner(System.in);
-	Random rand = new Random();
-
 	public Library() {
 		name = "Brookfield Public Library";
 
 		for (int i = 0; i < 10; i++) {
-			System.out.println("Enter the name of the book");
-			String title = input.nextLine();
+			String title = "The Selection";
+			Random rand = new Random();
 			int numPages = 10 + rand.nextInt(491);
 			fiction.add(new Book(title, numPages));
 		}
@@ -27,18 +24,12 @@ public class Library {
 		fiction = xFiction;
 	}
 
-	public void checkOut(String name) {
-		System.out.println("Enter the name of the book you want to check out");
-		String book = input.nextLine();
-		int numPages = 10 + rand.nextInt(491);
-		fiction.remove(new Book(book, numPages));
+	public void checkOut(int index) {
+		fiction.remove(index);
 	}
 
-	public void returnBook(String name) {
-		System.out.println("Enter the name of the book you want to return");
-		String book = input.nextLine();
-		int numPages = 10 + rand.nextInt(491);
-		fiction.add(new Book(book, numPages));
+	public void returnBook(String xName, int xNumPages) {
+		fiction.add(new Book(xName, xNumPages));
 	}
 
 	public void fire() {
