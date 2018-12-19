8688b502928cfa6532f0f7d5c2d15d4b5480e05a
19sathid <19sathid@elmbrookstudents.org>
2018-12-19 06:12:01 -0600

    Student

---
+++ src/classes/Student.java
@@ -0,0 +1,16 @@
+package classes;
+
+public class Student {
+private String name;
+private int[] tests;
+private int howMany;
+
+public Student() {
+	name = "AP Computer Science";
+}
+Scanner input = new Scanner(System.in);
+public void addaScore(int score) {
+	System.out.println("Enter the name of the student");
+	String stud = input.nextLine();
+}
+}
