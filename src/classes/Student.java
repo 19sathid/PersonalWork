c8ce042e07757b94c2cd424b545d84ca96d9fc66
19sathid <19sathid@elmbrookstudents.org>
2018-12-19 15:08:43 -0600

    Student

--- src/classes/Student.java
+++ src/classes/Student.java
@@ -1,16 +1,22 @@
 package classes;
-
+import java.util.ArrayList;
 public class Student {
 private String name;
-private int[] tests;
+private int[] tests ;
 private int howMany;
 
 public Student() {
 	name = "AP Computer Science";
 }
-Scanner input = new Scanner(System.in);
-public void addaScore(int score) {
-	System.out.println("Enter the name of the student");
-	String stud = input.nextLine();
+
+public Student(String n, int[] x, int y){
+	name = n;
+	tests = x;
+	howMany = y;
+}
+
+public void addaScore(String xScore) {
+	int score = Integer.parseInt(xScore);
+	tests.add
 }
 }
