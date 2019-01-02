fd04db312949104ba42e5dc0275bca7dce61c04f
19sathid <19sathid@elmbrookstudents.org>
2019-01-02 15:04:08 -0600

    Reverse Order

---
+++ src/recursion/ReverseOrder.java
@@ -0,0 +1,27 @@
+package recursion;
+
+import java.util.Scanner;
+
+public class ReverseOrder {
+	public static void reverse(int x) {
+		if (x > 0) {
+			int num = x % 10;
+			System.out.print(num);
+			reverse(x / 10);
+		}
+	}
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter a number");
+		int num = input.nextInt();
+		while (num < 0 || num == 0) {
+			System.out.println("Try again! Enter a valid number");
+			num = input.nextInt();
+		}
+		reverse(num);
+
+		input.close();
+	}
+
+}
