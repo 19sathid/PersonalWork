783aaec1eaf5a77a5b78b5d5b7660f9dc09ccfd9
19sathid <19sathid@elmbrookstudents.org>
2019-01-09 04:54:53 -0600

    Animal

---
+++ src/interfacesandpolymorphism/TestAnimal.java
@@ -0,0 +1,14 @@
+package interfacesandpolymorphism;
+import java.util.ArrayList;
+public class TestAnimal {
+
+	public static void main(String[] args) {
+		ArrayList<Animal> dogs = new ArrayList<Animal>();
+		dogs.add(new Dog());
+		
+		for(Animal x : dogs) {
+			
+		}
+	}
+
+}
