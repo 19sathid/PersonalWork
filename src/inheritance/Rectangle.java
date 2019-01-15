b7d549b7796e64abc0ef0254e97bb483f650f945
19sathid <19sathid@elmbrookstudents.org>
2019-01-15 14:31:08 -0600

    Rectangle

--- src/inheritance/Rectangle.java
+++ src/inheritance/Rectangle.java
@@ -1,16 +1,20 @@
 package inheritance;
 
-public class Rectangle extends Parallelogram implements Geo{
+public class Rectangle extends Parallelogram implements Geo {
 	public Rectangle() {
-		super(6, 12);
+		super(6, 12, 6);
 	}
 
-	public Rectangle(double length, double width) {
-		super(length, width);
+	public Rectangle(double length, double width, double height) {
+		super(length, width, width);
 	}
 
 	public double area() {
 		return super.getSide1() * super.getSide2();
+	}
+
+	public String toString() {
+		return ("Rectangle : " + super.toString());
 	}
 
 }
