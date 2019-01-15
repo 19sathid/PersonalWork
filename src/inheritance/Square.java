7ab45bbe0dfae56e15b7b88e4d122b1a31e461fe
19sathid <19sathid@elmbrookstudents.org>
2019-01-15 14:31:16 -0600

    Square

--- src/inheritance/Square.java
+++ src/inheritance/Square.java
@@ -3,10 +3,10 @@ package inheritance;
 public class Square extends Rectangle{
 
 	public Square(){
-		super(6, 6);
+		super(6, 6, 6);
 	}
 	
 	public Square(double side){
-		super(side, side);
+		super(side, side, side);
 	}
 }
