62f4f817a4478cf40625c65c1350447de6692189
19sathid <19sathid@elmbrookstudents.org>
2019-01-15 15:05:37 -0600

    Test Rectangle

--- src/inheritance/TestRectangle.java
+++ src/inheritance/TestRectangle.java
@@ -3,7 +3,7 @@ package inheritance;
 public class TestRectangle {
 
 	public static void main(String[] args) {
-		Rectangle box = new Rectangle(10, 20);
+		Rectangle box = new Rectangle(10, 20, 10);
 		
 		System.out.println("Perimeter of box : " + box.perimeter());
 		System.out.println("Area of box : " + box.area());
